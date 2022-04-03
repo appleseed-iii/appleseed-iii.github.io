d17d516c34aca15b7ed48faf1ca6a5a2106726ab
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2022-04-03 15:03:03 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export default {
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  // rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'Appleseed',
