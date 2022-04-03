0270a933082c4dfcfc94e287728525cfb870a43b
appleseed-iii <80423742+appleseed-iii@users.noreply.github.com>
2022-04-03 14:58:47 -0500

    debugging why builds are failing

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ export default {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Applebee\'s',
+  name: 'Appleseed',
   domain: 'appleseed-iii.github.io',
-  author: 'Appleseed',
+  author: 'Applebees',
 
   // open graph metadata (optional)
   description: 'Appleseed',
